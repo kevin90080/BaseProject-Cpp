001989b549d943a459fa298065bebd463521d7a9
PC234-26\PC234 <kevin90080@yahoo.com.tw>
2017-12-22 18:58:29 +0800

    補做

--- test/TestMain.cpp
+++ test/TestMain.cpp
@@ -3,6 +3,12 @@
 
 void testGetTriangleType() {
     assert(getTriangleType(3, 4, 5) == 0);
+    assert(getTriangleType(5, 4, 3) == 0);
+    assert(getTriangleType(3, 4, 4) == 1);
+
+    assert(getTriangleType(1, 2, 3) == -2);
+    assert(getTriangleType(3, 4, 6) == -1);
+    assert(getTriangleType(3, 3, 3) == 1);
 }
 
 int main() {
